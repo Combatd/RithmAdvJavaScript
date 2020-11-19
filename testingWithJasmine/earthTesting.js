8db681656395d3f9614950beaea3c0c522af098e
Combatd <combatdstar@gmail.com>
2020-11-18 23:28:27 -0800

    Test Arrays

--- testingWithJasmine/earthTesting.js
+++ testingWithJasmine/earthTesting.js
@@ -17,16 +17,55 @@ let earth = {
 // it "is the densest of all the planets"
 // expect(earth.density).toBeGreaterThan(5.51)
 
-describe("Earth", () => {
-    it("is around", () => {
-        expect(earth.isRound).toEqual(true);
-    });
+// describe("Earth", () => {
+//     it("is around", () => {
+//         expect(earth.isRound).toEqual(true);
+//     });
 
-    it("is the third planet from the sun", () => {
-        expect(earth.numberFromSun).toEqual(3);
-    });
+//     it("is the third planet from the sun", () => {
+//         expect(earth.numberFromSun).toEqual(3);
+//     });
 
-    it("is the densest of all the planets", () => {
-        expect(earth.density).toBeGreaterThan(5.5);
+//     it("is the densest of all the planets", () => {
+//         expect(earth.density).toBeGreaterThan(5.5);
+//     });
+// });
+
+let arr;
+beforeEach(function () {
+    arr = [1, 3, 5];
+});
+
+describe("Arrays", function () {
+    describe("#push", function () {
+        it("adds elements to an array", function () {
+            arr.push(7);
+            expect(arr).toEqual([1, 3, 5, 7]);
+        });
+        it("returns the new length of the array", function () {
+            expect(arr.push(7)).toBe(4);
+        });
+        it("adds anything into the array", function () {
+            expect(arr.push({})).toBe(4);
+        });
     });
 });
+
+// WITHOUT running a callback with beforeEach requires initializing arr in each block
+// describe("Arrays", function () {
+//     describe("#push", function () {
+//         it("adds elements to an array", function () {
+//             let arr = [1, 3, 5];
+//             arr.push(7);
+//             expect(arr).toEqual([1, 3, 5, 7]);
+//         });
+//         it("returns the new length of the array", function () {
+//             var arr = [1, 3, 5];
+//             expect(arr.push(7)).toBe(4);
+//         });
+//         it("adds anything into the array", function () {
+//             var arr = [1, 3, 5];
+//             expect(arr.push({})).toBe(4);
+//         });
+//     });
+// });
