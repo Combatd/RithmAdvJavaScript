5cb997b3d55c45f8eb228e33115f1f24bc5f2d37
Combatd <combatdstar@gmail.com>
2020-11-19 17:13:45 -0800

    Describe replaceWith in testingSpec.js

---
+++ testingWithJasmine/testingSpec.js
@@ -0,0 +1,27 @@
+// describe("onlyStrings", function () {
+//     it("returns an array", function () {
+//         expect(onlyStrings([1, 2, 3])).toEqual(jasmine.any(Array));
+//     });
+//     it("does not change arrays of strings", function () {
+//         expect(onlyStrings(["a", "b", "c"])).toEqual(["a", "b", "c"]);
+//     });
+//     it("removes non-string primitives from an array", function () {
+//         expect(onlyStrings([1, "hi", null, "cool", undefined, "woah", false, "ok"])).toEqual(["hi", "cool", "woah", "ok"]);
+//     });
+//     it("removes reference types from an array", function () {
+//         expect(onlyStrings([{}, "a", [], "b", function () { }, "c"])).toEqual(["a", "b", "c"]);
+//     });
+// });
+
+/*
+Write a function called replaceWith that takes in a string, 
+a character to replace and a character to replace it with 
+and returns the string with the replacements. 
+Write tests to make sure this is case sensitive
+*/
+
+describe('replaceWith', () => {
+    it('takes in a string, a character to replace, and a character to replace it with as parameters, and returns a string with the replacements', () => {
+        expect(replaceWith("awesome", "e", "z").toEqual("awesomz"));
+    });
+});
