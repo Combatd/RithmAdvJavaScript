a54061dcedf87544dd8243d52166b181d06af234
Combatd <combatdstar@gmail.com>
2020-11-24 01:17:24 -0800

    Test #sumEvenArguments returns the sum of the even arguments

--- keywordThis/callApplyBindSpec.js
+++ keywordThis/callApplyBindSpec.js
@@ -9,7 +9,9 @@ describe('#sumEvenArguments', () => {
     });
 
     it('returns the sum of the even arguments', () => {
-
+        expect(sumEvenArguments(1,2,3,4)).toEqual(6);
+        expect(sumEvenArguments(1,2,6)).toEqual(8);
+        expect(sumEvenArguments(1,2)).toEqual(2);
     });
 });
 
