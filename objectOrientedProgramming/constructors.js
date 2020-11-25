453675436c07eee6ef842b9865ab13f7999ea40d
Combatd <combatdstar@gmail.com>
2020-11-24 17:35:11 -0800

    Define cconstructor func Person

---
+++ objectOrientedProgramming/constructors.js
@@ -0,0 +1,11 @@
+/*
+Create a constructor function for a Person. 
+Each person should have a firstName, lastName, favoriteColor and favoriteNumber.
+*/
+
+function Person(firstName, lastName, favoriteColor, favoriteNumber) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.favoriteColor = favoriteColor;
+    this.favoriteNumber = favoriteNumber;
+}
