629cc03a38b0ba5a9fc12e2dd79f36e27a8af5d1
Combatd <combatdstar@gmail.com>
2020-11-24 17:38:35 -0800

    Define Person::multiply(number)

--- objectOrientedProgramming/constructors.js
+++ objectOrientedProgramming/constructors.js
@@ -3,9 +3,20 @@ Create a constructor function for a Person.
 Each person should have a firstName, lastName, favoriteColor and favoriteNumber.
 */
 
+/*
+Write a method called multiplyFavoriteNumber that takes in a number 
+and returns the product of the number and the Person's favorite number
+*/
 function Person(firstName, lastName, favoriteColor, favoriteNumber) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.favoriteColor = favoriteColor;
     this.favoriteNumber = favoriteNumber;
+
+    this.multiply = (number) => {
+        return this.favoriteNumber * number;
+    }
 }
+
+let sakuraMiko = new Person('Miko', 'Sakura', 'Pink', 2);
+console.log(sakuraMiko.multiply(5));
