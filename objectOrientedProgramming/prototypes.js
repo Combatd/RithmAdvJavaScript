3802e22cf22d019eecad07e1d14fd9bb2dcf788a
Combatd <combatdstar@gmail.com>
2020-11-24 20:24:15 -0800

    Setup prototypes.js problem set

---
+++ objectOrientedProgramming/prototypes.js
@@ -0,0 +1,41 @@
+/*
+Create a constructor function for a Person. 
+Each person should have a firstName, lastName, favoriteColor, favoriteNumber and favoriteFoods 
+(which should be an array).
+
+Add a function on the Person.prototype called fullName that 
+returns the firstName and lastName property of an object created by the Person constructor concatenated together.
+*/
+
+
+
+
+
+/*
+Overwrite the toString method from the Object prototype by creating a toString method for Person. 
+The toString method should return a string in the following format:
+*/
+
+
+
+
+
+/*
+Add a property on the Person object called family which is an empty array.
+
+Add a function on the Person.prototype called addToFamily which adds an object constructed from the Person constructor to the family array. 
+To make sure that the object you are adding is an object construced from the Person constructor - take a look at the instanceofoperator. 
+Make sure that your family array does not include duplicates! This method should return the length of the family array.
+*/
+
+
+
+
+
+/*
+Implement your own version of Array.prototype.map
+
+Implement a function that reverses a string and place it on the String.prototype
+
+Implement your own version of Function.prototype.bind
+*/
