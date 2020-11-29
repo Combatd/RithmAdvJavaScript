e3953cb3ad3c0a3e3dc53f0c3226890130ada0c2
Combatd <combatdstar@gmail.com>
2020-11-29 00:22:37 -0800

    Define #add(a,b)

--- ES2015/es2015exercises.js
+++ ES2015/es2015exercises.js
@@ -76,3 +76,15 @@ let a, b = obj.numbers // object destructuring
 //     }
 //     return a+b
 // }
+
+const add = (a, b) => {
+    if (a === 0) { a = 0 }
+    else {
+        a = a || 10;
+    }
+    if (b === 0) b = 0
+    else {
+        b = b || 10;
+    }
+    return a * b;
+}
