06936e8afb229d56636b6b1263451cc5ddb9ecf7
Combatd <combatdstar@gmail.com>
2020-11-29 19:10:26 -0800

    Define #inOrder using callbacks

---
+++ managingAsyncCode/asyncExercise.js
@@ -0,0 +1,27 @@
+// Write a function called inOrder that accepts two callbacks and invokes them in order. 
+// Implement inOrder using the callback pattern.
+
+const inOrder = (fn1, fn2) => {
+    if (fn1) {
+        fn1
+    } else {
+        throw("arguments must be a function");
+    }
+
+    if (fn2) {
+        fn2
+    } else {
+        throw("arguments must be a function");
+    }
+}
+
+
+var logOne = setTimeout(function() {
+    console.log("one!");
+}, Math.random() * 3000);
+  
+var logTwo = setTimeout(function() {
+    console.log("two!");
+}, Math.random() * 1000);
+  
+inOrder(logOne, logTwo);
