d7d213f541ab1df277308eb6eaa9d3c189234eaa
Combatd <combatdstar@gmail.com>
2020-12-01 15:36:31 -0800

    Import Nothing Special problem

---
+++ regularExpressions/exercises.js
@@ -0,0 +1,17 @@
+/*
+The notorious Captain Schneider has given you a very straight forward mission. 
+Any data that comes through the system make sure that only non-special characters see the light of day.
+
+Create a function that given a string, retains only the letters A-Z (upper and lowercase), 
+0-9 digits, and whitespace characters. 
+Also, returns "Not a string!" if the entry type is not a string.
+*/
+
+function nothingSpecial(str) {
+    
+}
+
+nothingSpecial("Hello World!"); // => "Hello World")
+nothingSpecial("%^Take le$ft ##quad%r&a&nt"); // => "Take left quadrant")
+nothingSpecial("M$$$$$$$y ally!!!!!"); // => "My ally")
+nothingSpecial(25); // => "Not a string!"
