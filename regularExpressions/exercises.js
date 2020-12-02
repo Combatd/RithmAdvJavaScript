7f438d76360afcc0d221cced2377069ba99bfb91
Combatd <combatdstar@gmail.com>
2020-12-01 21:45:24 -0800

    Define #nothingSpecial

--- regularExpressions/exercises.js
+++ regularExpressions/exercises.js
@@ -5,13 +5,22 @@ Any data that comes through the system make sure that only non-special character
 Create a function that given a string, retains only the letters A-Z (upper and lowercase), 
 0-9 digits, and whitespace characters. 
 Also, returns "Not a string!" if the entry type is not a string.
+
+^ means everything that is not matching the expression
+0-9
+A-Z
+a-z
+\s whitespace
 */
 
 function nothingSpecial(str) {
-    
+    if (typeof(str) !== 'string') { return 'Not a string!' }
+    // let notNormalChars = new RegExp('^[0-9][A-Z][a-z]\s');
+    let notNormalChars = /[^0-9A-Za-z\s]/g;
+    return str.replace(notNormalChars, '');
 }
 
-nothingSpecial("Hello World!"); // => "Hello World")
-nothingSpecial("%^Take le$ft ##quad%r&a&nt"); // => "Take left quadrant")
-nothingSpecial("M$$$$$$$y ally!!!!!"); // => "My ally")
-nothingSpecial(25); // => "Not a string!"
+console.log(nothingSpecial("Hello World!")); // => "Hello World")
+console.log(nothingSpecial("%^Take le$ft ##quad%r&a&nt")); // => "Take left quadrant")
+console.log(nothingSpecial("M$$$$$$$y ally!!!!!")); // => "My ally")
+console.log(nothingSpecial(25)); // => "Not a string!"
